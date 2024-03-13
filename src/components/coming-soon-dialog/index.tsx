526b16b9fbcdf20d0cef01417dbe1312af579c2d
Prateek Goyal <gprateek015@gmail.com>
2024-03-13 22:44:53 +0530

    comming soon model update

--- src/components/coming-soon-dialog/index.tsx
+++ src/components/coming-soon-dialog/index.tsx
@@ -3,6 +3,8 @@
 import { Dialog, Grid, Typography } from '@mui/material';
 import React, { useContext } from 'react';
 import { comingSoonDialogContext } from '../providers';
+import RocketLaunchIcon from '@/assets/launching-soon-icon.png';
+import Image from 'next/image';
 
 const ComingSoonDialog = () => {
   const { comingSoonDialogOpen, setComingSoonDialogOpen } = useContext(
@@ -17,13 +19,40 @@ const ComingSoonDialog = () => {
       onClose={onClose}
       PaperProps={{
         sx: {
-          background: 'transparent',
-          boxShadow: 'none'
+          borderRadius: '20px'
         }
       }}
     >
-      <Grid>
-        <Typography variant='h3'>Launching soon!</Typography>
+      <Grid
+        p='40px 60px'
+        sx={{
+          background: 'rgba(255,255,255,0.9)',
+          display: 'flex',
+          alignItems: 'center',
+          gap: '10px',
+          flexDirection: 'column'
+        }}
+      >
+        <Image
+          src={RocketLaunchIcon}
+          alt='launching soon'
+          style={{
+            width: '150px',
+            height: '150px'
+          }}
+        />
+        <Typography
+          sx={theme => ({
+            fontSize: '40px',
+            lineHeight: '44px',
+            color: theme.palette.grey[600],
+            textTransform: 'uppercase',
+            fontWeight: '700',
+            letterSpacing: '3px'
+          })}
+        >
+          Launching soon!
+        </Typography>
       </Grid>
     </Dialog>
   );
